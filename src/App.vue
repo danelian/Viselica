7b44765a407f0a721a94ba67c59c535c268581fc
danelian <141317269+danelian@users.noreply.github.com>
2023-10-07 10:59:00 +0300

    Логика и реализация
    1. Есть возможность вводить только русские буквы
    2. Есть возможность только 6 ошибок. Отрисовывается фигура.
    3. При повторном вводе буквы появляется сообщение об этом.
    4. При проигрыше или выигрыше появляется попап.
    5. Есть список с ошибками, и есть вывод слова с появлением при угадывании.

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,60 @@ import GameWrongLetters from './components/GameWrongLetters.vue';
 import GameWord from './components/GameWord.vue';
 import GamePopup from './components/GamePopup.vue';
 import GameNotification from './components/GameNotification.vue';
+import { computed, ref, watch } from 'vue';
+
+const word = ref('василий')
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter(x => word.value.includes(x)))
+const wrongLetters = computed(() => letters.value.filter(x => !word.value.includes(x)))
+const isLose = computed(() => wrongLetters.value.length === 6)
+const isWin = computed(() => [...word.value].every((x) => correctLetters.value.includes(x)))
+const notification = ref<InstanceType<typeof GameNotification> | null>(null)
+const popup = ref<InstanceType<typeof GamePopup> | null>(null)
+
+watch(wrongLetters, () => {
+  if (isLose.value) {
+    popup.value?.open('lose')
+  }
+})
+
+watch(correctLetters, () => {
+  if (isWin.value) {
+    popup.value?.open('win')
+  }
+})
+
+window.addEventListener('keydown', ({ key }) => {
+  if (isLose.value || isWin.value) {
+    return
+  }
+
+  if (letters.value.includes(key)) {
+    notification.value?.open()
+    setTimeout(() => notification.value?.close(), 2000)
+    return
+  }
+
+  if (/[a-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase())
+  }
+})
+
+const restart = () => {
+  letters.value = []
+  popup.value?.close()
+}
 </script>
 
 <template>
   <GameHeader/>
 
   <div class="game-container">
-    <GameFigure/>
-    <GameWrongLetters/>
-    <GameWord/>
+    <GameFigure :wrong-letters-count="wrongLetters.length"/>
+    <GameWrongLetters :wrong-letters="wrongLetters"/>
+    <GameWord :word="word" :correct-letters="correctLetters" />
   </div>
   
-  <GamePopup/>
-  <GameNotification/>
+  <GamePopup ref="popup" :word="word" @restart="restart"/>
+  <GameNotification ref="notification"/>
 </template>
